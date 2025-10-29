66263921226f9316f4cf14399d52763b04669294
Jacob Carpenter <github@jacobcarpenter.com>
2025-10-29 12:59:06 -0700

    add production profiling support
    ```
    node run build -- --mode profiling
    node run preview
    ```

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
 	base: "/t2k4-specialties/",
 	plugins: [
 		react({
@@ -13,4 +12,11 @@ export default defineConfig({
 		}),
 		tailwindcss(),
 	],
-});
+	resolve: {
+		alias: {
+			...(mode === "profiling" && {
+				"react-dom/client": "react-dom/profiling",
+			}),
+		},
+	},
+}));
